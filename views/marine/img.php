9be7bda3837f50d806c7257defe2217c4ee10669
katyan-averr <85882133+katyan-averr@users.noreply.github.com>
2021-10-09 00:27:16 +0800

    опа всё работает

--- views/marine/img.php
+++ views/marine/img.php
@@ -1,1 +1,8 @@
-<img src="/imges/marine.jpg" alt="">
+<img class="main-image-wrapper" src="https://i.ytimg.com/vi/5NbhVWTl2Mc/maxresdefault.jpg" alt="">
+
+<style>
+    .main-image-wrapper {
+            width: 640px; 
+            height: 360px; 
+    }
+</style>
--- views/pirates/img.php
+++ views/pirates/img.php
@@ -1,1 +1,8 @@
-<img src="/imges/pirates.jpg" alt="">
+<img class="main-image-wrapper" src="https://pbs.twimg.com/media/EkdM3aEXIAAAbUb.jpg" alt="">
+
+<style>
+    .main-image-wrapper {
+            width: 500px; 
+            height: 355px; 
+    }
+</style>
