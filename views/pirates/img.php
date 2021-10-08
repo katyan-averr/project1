a4e21d34ad7518a5207c0309d6716f1e30e6c773
katyan-averr <85882133+katyan-averr@users.noreply.github.com>
2021-10-08 22:59:46 +0800

    cheto poluchaetsa

---
+++ views/pirates/img.php
@@ -0,0 +1,18 @@
+<div class="mt-4 main-image-wrapper">
+    <img class="main-image" src="/imges/.jpg" alt="">
+</div>
+<style>
+    .main-image-wrapper {
+            text-align: center;
+            width: 250px; 
+            height: 250px; 
+            border: 1px solid silver;
+            margin: 8px;
+            box-shadow: 0 0 16px silver;
+            border: 5px solid white;
+            overflow: hidden;
+    }
+    .main-image {
+            height:100%;
+        }
+</style>
